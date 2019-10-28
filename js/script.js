daa5ea56f368e6958216a9e3c684b627b9861ce7
nwagalla1 <nixonwagallah@gmail.com>
2019-10-28 09:20:58 +0300

    added the if function

--- js/script.js
+++ js/script.js
@@ -21,4 +21,13 @@ submit.addEventListener("click", function display() {
   
     const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
     ``;
-    
+    if (dd < 1 && dd < 31) {
+        display.style.background = "red";
+        display.style.color = "#fff";
+        display.innerHTML = `Fala, enter a valid date`;
+      } else if (male.checked) {
+        display.innerHTML = `Hello Papagena, you were born on a ${days[dayOfBirth]} and your Akan name is ${maleNames[dayOfBirth]} `;
+      } else {
+        display.innerHTML = `Bonjour Maidemoiselle, you were born on a ${days[dayOfBirth]} and your Akan name is ${femaleNames[dayOfBirth]} `;
+      }
+    });
